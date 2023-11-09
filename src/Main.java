b84e0d3a0a0645b845229d889cab5033f4ae2676
Larisa <laserova@yandex.ru>
2023-11-09 19:42:26 +0500

    task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       String a = "aadrsadda";
+        Map<Character,Integer> countChar=new HashMap<>();
+       for (int i=0;i<a.length();i++){
+          if(countChar.containsKey(a.charAt(i)){
+              countChar.put(a.charAt(i),countChar.get(a.charAt(i))+1);
+           } else{
+              countChar.put(a.charAt(i),1);
+           }
+       }
     }
 }
